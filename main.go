8babe174f6ac0449d312243b9f6003af64b519c3
yolossn <nssvlr@gmail.com>
2020-09-17 19:35:40 +0530

    print status of processes in example

--- main.go
+++ main.go
@@ -28,7 +28,15 @@ func main() {
 
 	fmt.Println("Successful process count: ", man.SuccessCount())
 	fmt.Println("Failed process count: ", man.FailCount())
-
+	status := man.Status()
+	for k, v := range status {
+		fmt.Println("-----")
+		fmt.Println("command:", k)
+		fmt.Println("isSuccessful:", v.IsSuccessful)
+		fmt.Println("output:", v.Output)
+		fmt.Println("error:", v.Err)
+		fmt.Println("-----")
+	}
 	// Done
 	fmt.Println("done", time.Since(now))
 }
