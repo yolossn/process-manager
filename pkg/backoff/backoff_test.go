85aa2b3b1be6f115f116ccc065e85c0549958da7
yolossn <nssvlr@gmail.com>
2020-09-17 13:59:53 +0530

    convert backoff test to blackbox test

--- pkg/backoff/backoff_test.go
+++ pkg/backoff/backoff_test.go
@@ -1,14 +1,15 @@
-package backoff
+package backoff_test
 
 import (
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
+	"github.com/yolossn/process-manager/pkg/backoff"
 )
 
 func TestNewStaticBackoff(t *testing.T) {
-	bo := NewStaticBackoff(time.Second)
+	bo := backoff.NewStaticBackoff(time.Second)
 
 	sleep := bo.Duration()
 	require.Equal(t, time.Second, sleep)
